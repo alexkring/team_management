89a13e6fbe7d9ea0176ec4c27aff1d3940538bf5
Alexander w Kring <alexkring@Alexanders-MacBook-Pro.local>
2024-07-25 19:20:55 -0700

    functional server and client working together. also updated gitignore.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './HelloWorld.js';
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <HelloWorld>
+      </HelloWorld>
     </div>
   );
 }
