d76f25ce6c1023ae25a25dab0a1ee2bec5ef57d4
Alexander w Kring <alexkring@Alexanders-MacBook-Pro.local>
2024-07-27 13:20:56 -0700

    Adding user icon and styling

--- client/src/ListPage.js
+++ client/src/ListPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import addUserIcon from './assets/plus.png';
+import userIcon from './assets/user.png';
 import './ListPage.css';
 
 function ListPage() {
@@ -17,19 +18,25 @@ function ListPage() {
   }, []);
 
   const renderedUsers = users.map((user) =>
-    <div key={user.id} className="flex justify-between p-2 text-lg text-base">
-      <div> First name: {user.first_name} </div>
-      <div> Last name: {user.last_name} </div>
-      <div> Email: {user.email} </div>
-      <div> Phone Number: {user.phone_number} </div>
-      <div> Role: {user.role} </div>
+    <div key={user.id} className="ListPage-user">
+      <div className="ListPage-user-icon">
+        <img src={userIcon} alt="Add User" className="ListPage-user-icon-image" />
+      </div>
+      <div className="ListPage-user-info">
+        <div> First name: {user.first_name} </div>
+        <div> Last name: {user.last_name} </div>
+        <div> Email: {user.email} </div>
+        <div> Phone Number: {user.phone_number} </div>
+        <div> Role: {user.role} </div>
+      </div>
+      <div className="ListPage-user-footer"> </div>
     </div>
   );
 
   return (
     <div className="ListPage">
       <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-          <img src={addUserIcon} alt="Add User" className="ListPage-add-user-icon" />
+        <img src={addUserIcon} alt="Add User" className="ListPage-add-user-icon" />
       </div>
       <h1>Team Members</h1>
       <p>You have {users.length} team members</p>
