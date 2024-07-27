2f9b9463e5bb3a96539ab5395778d431a7bf9624
Alexander w Kring <alexkring@Alexanders-MacBook-Pro.local>
2024-07-26 19:17:34 -0700

    Change app to just show listpage to start

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import HelloWorld from './HelloWorld.js';
 import ListPage from './ListPage.js';
 import AddPage from './AddPage.js';
 import EditPage from './EditPage.js';
@@ -9,27 +8,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-      <HelloWorld>
-      </HelloWorld>
       <ListPage>
       </ListPage>
-      <AddPage>
-      </AddPage>
-      <EditPage>
-      </EditPage>
+      <footer className="App-footer">
+      </footer>
     </div>
   );
 }
