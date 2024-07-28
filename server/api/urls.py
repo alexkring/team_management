dc4bf0343c15be55b514e854c0157428ab84b679
Alexander w Kring <alexkring@Alexanders-MacBook-Pro.local>
2024-07-28 08:34:56 -0700

    cleaning up backend code

--- server/api/urls.py
+++ server/api/urls.py
@@ -7,10 +7,6 @@ router = DefaultRouter()
 router.register(r'users', UserViewSet) 
 
 urlpatterns = [
-    path('hello-world/', views.hello_world, name='hello_world'),
-    path('list-page/', views.list_page, name='list_page'),
-    path('add-page/', views.add_page, name='add_page'),
-    path('edit-page/', views.edit_page, name='edit_page'),
     path('user/', views.user, name='user'),
     path('', include(router.urls)),
 ]
--- server/api/views.py
+++ server/api/views.py
@@ -7,37 +7,6 @@ from .serializers import *
 
 # Create your views here.
 
-@api_view(['GET'])
-def hello_world(request):
-    return Response({'message': 'Hello, world!'})
-
-@api_view(['GET'])
-def list_page(request):
-    users = [{
-        'id': '1',
-        'first_name': 'Alex',
-        'last_name': 'Kring',
-        'email': 'awkring@gmail.com',
-        'phone_number': '415-215-2135',
-        'role': 'admin'
-    }, {
-        'id': '2',
-        'first_name': 'Jeff',
-        'last_name': 'Jones',
-        'email': 'jeff@gmail.com',
-        'phone_number': '605-305-6115',
-        'role': 'team_member'
-    }]
-    return Response({'users': users})
-
-@api_view(['GET'])
-def add_page(request):
-    return Response({'message': 'add page backend response.'})
-
-@api_view(['GET'])
-def edit_page(request):
-    return Response({'message': 'edit page backend response.'})
-
 @api_view(['POST'])
 def user(request):
     user_object = request.data['user_object']
